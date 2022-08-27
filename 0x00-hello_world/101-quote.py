95aa7e7ad7e7ad258551355da27b01044f7e3c4a
oluwatobi-fasasi <wasiufasasi79@gmail.com>
2022-08-26 23:28:12 -0400

    done

---
+++ 0x00-hello_world/101-quote.py
@@ -0,0 +1,4 @@
+def quote():
+    print('and that piece of art is useful" - Dora Korpar, 2015-10-19')
+    return 1
+quote()
---
+++ 0x00-hello_world/5-print.py
@@ -0,0 +1,4 @@
+def printf():
+    print('with proper grammar, but the outcome is a piece of art')
+    return 0
+printf()
