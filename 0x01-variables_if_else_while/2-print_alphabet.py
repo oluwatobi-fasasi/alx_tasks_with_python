5a6e8a8232225600a38ac85cb55fafd1070eb324
oluwatobi-fasasi <wasiufasasi79@gmail.com>
2022-08-27 00:32:58 -0400

    insomnia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.py
@@ -0,0 +1,7 @@
+def lowerCase(l):
+    print(l.lower())
+
+print('Type only alphabet letters in UPPERCASE to change  it to LOWERCASE')
+letters = input()
+lowerCase(letters)
+
---
+++ 0x01-variables_if_else_while/3-print_alphabets.py
@@ -0,0 +1,6 @@
+def lowerUpper(l):
+    print(l.lower() + l.upper())
+
+print('Type any letters to get LOWERCASE and UPPERCASE...')
+letters = input()
+lowerUpper(letters)
