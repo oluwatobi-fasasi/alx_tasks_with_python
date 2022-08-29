bc849fed77fbf6172fb5d519b4366d4c9656d4fe
oluwatobi-fasasi <wasiufasasi79@gmail.com>
2022-08-29 01:41:12 -0400

    fixed comman in last figure

--- 0x01-variables_if_else_while/101-print_comb4.py
+++ 0x01-variables_if_else_while/101-print_comb4.py
@@ -2,7 +2,9 @@ def comb4():
     for i in range(10):
         for j in range(10):
             for k in range(10):
-                if i < j and j < k:
+                if i == 7 and j == 8 and k == 9:
+                    print(str(i) + str(j) + str(k))
+                elif i < j and j < k:
                     print(str(i) + str(j) + str(k) + ', ', end ='')
 
 comb4()
